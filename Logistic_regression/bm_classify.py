046c6f6ae0df9853f297af7cecf8078ca722b1f9
Zaiyan Alam <alamm@usc.edu>
2020-10-08 04:05:49 -0700

    Add files via upload

---
+++ Logistic_regression/bm_classify.py
@@ -0,0 +1,205 @@
+import numpy as np
+
+
+def binary_train(X, y, loss="perceptron", w0=None, b0=None, step_size=0.5, max_iterations=1000):
+    """
+    Inputs:
+    - X: training features, a N-by-D numpy array, where N is the
+    number of training points and D is the dimensionality of features
+    - y: binary training labels, a N dimensional numpy array where
+    N is the number of training points, indicating the labels of
+    training data
+    - loss: loss type, either perceptron or logistic
+    - step_size: step size (learning rate)
+	- max_iterations: number of iterations to perform gradient descent
+
+    Returns:
+    - w: D-dimensional vector, a numpy array which is the weight
+    vector of logistic or perceptron regression
+    - b: scalar, which is the bias of logistic or perceptron regression
+    """
+    N, D = X.shape
+    assert len(np.unique(y)) == 2
+    w = np.zeros(D)
+    if w0 is not None:
+        w = w0
+    b = 0
+    if b0 is not None:
+        b = b0
+
+    step_size /= N
+    y_new_val = np.where(y == 0, -1, 1)
+
+    if loss == "perceptron":
+        w = np.zeros(D)
+        b = 0
+        for each in range(max_iterations):
+            loss_val = y_new_val * (X.dot(w) + b)
+            loss_val = np.int32(loss_val <= 0)
+            loss_val = loss_val * y_new_val
+            updated_weight = loss_val.T.dot(X)
+            w += step_size*updated_weight
+            b += step_size*np.sum(loss_val)
+
+    elif loss == "logistic":
+        w = np.zeros(D)
+        b = 0
+        for each in range(max_iterations):
+            loss_val = -y_new_val * (X.dot(w) + b)
+            loss_val = sigmoid(loss_val) * y_new_val
+            updated_weight = loss_val.T.dot(X)
+            w += step_size*updated_weight
+            b += step_size*np.sum(loss_val)
+
+    else:
+        raise "Loss Function is undefined."
+
+    assert w.shape == (D,)
+    return w, b
+
+def sigmoid(z):
+
+    """
+    Inputs:
+    - z: a numpy array or a float number
+
+    Returns:
+    - value: a numpy array or a float number after computing sigmoid function value = 1/(1+exp(-z)).
+    """
+
+    ############################################
+    # TODO 3 : Edit this part to               #
+    #          Compute value                   #
+    sig_val = z
+    ############################################
+    temp=np.exp(-z)
+    sig_val = 1/(1+temp)
+    return sig_val
+
+def binary_predict(X, w, b, loss="perceptron"):
+    """
+    Inputs:
+    - X: testing features, a N-by-D numpy array, where N is the
+    number of training points and D is the dimensionality of features
+    - w: D-dimensional vector, a numpy array which is the weight
+    vector of your learned model
+    - b: scalar, which is the bias of your model
+    - loss: loss type, either perceptron or logistic
+
+    Returns:
+    - preds: N dimensional vector of binary predictions: {0, 1}
+    """
+    N, D = X.shape
+
+    if loss == "perceptron":
+        ############################################
+        # TODO 4 : Edit this if part               #
+        #          Compute preds                   #
+        #preds = np.zeros(N)
+        ############################################
+        temp = (X.dot(w) + b)
+        predictions = np.int32(temp > 0)
+
+
+    elif loss == "logistic":
+        ############################################
+        # TODO 5 : Edit this if part               #
+        #          Compute preds                   #
+        #preds = np.zeros(N)
+        ############################################
+        temp = (X.dot(w) + b)
+        predictions = np.int32(temp > 0)
+
+    else:
+        raise "Loss Function is undefined."
+
+
+    assert predictions.shape == (N,)
+    return predictions
+
+
+
+def multiclass_train(X, y, C,
+                     w0=None,
+                     b0=None,
+                     gd_type="sgd",
+                     step_size=0.5,
+                     max_iterations=1000):
+    """
+    Inputs:
+    - X: training features, a N-by-D numpy array, where N is the
+    number of training points and D is the dimensionality of features
+    - y: multiclass training labels, a N dimensional numpy array where
+    N is the number of training points, indicating the labels of
+    training data
+    - C: number of classes in the data
+    - gd_type: gradient descent type, either GD or SGD
+    - step_size: step size (learning rate)
+    - max_iterations: number of iterations to perform gradient descent
+
+    Returns:
+    - w: C-by-D weight matrix of multinomial logistic regression, where
+    C is the number of classes and D is the dimensionality of features.
+    - b: bias vector of length C, where C is the number of classes
+    """
+
+    N, D = X.shape
+
+    w = np.zeros((C, D))
+    if w0 is not None:
+        w = w0
+
+    b = np.zeros(C)
+    if b0 is not None:
+        b = b0
+
+    np.random.seed(42)
+    if gd_type == "sgd":
+        ############################################
+        # TODO 6 : Edit this if part               #
+        #          Compute w and b                 #
+        w = np.zeros((C, D))
+        b = np.zeros(C)
+        ############################################
+
+
+    elif gd_type == "gd":
+        ############################################
+        # TODO 7 : Edit this if part               #
+        #          Compute w and b                 #
+        w = np.zeros((C, D))
+        b = np.zeros(C)
+        ############################################
+
+
+    else:
+        raise "Type of Gradient Descent is undefined."
+
+
+    assert w.shape == (C, D)
+    assert b.shape == (C,)
+
+    return w, b
+
+
+def multiclass_predict(X, w, b):
+    """
+    Inputs:
+    - X: testing features, a N-by-D numpy array, where N is the
+    number of training points and D is the dimensionality of features
+    - w: weights of the trained multinomial classifier, C-by-D
+    - b: bias terms of the trained multinomial classifier, length of C
+
+    Returns:
+    - preds: N dimensional vector of multiclass predictions.
+    Outputted predictions should be from {0, C - 1}, where
+    C is the number of classes
+    """
+    N, D = X.shape
+    ############################################
+    # TODO 8 : Edit this part to               #
+    #          Compute preds                   #
+    ############################################
+
+    assert predictions.shape == (N,)
+    return predictions
